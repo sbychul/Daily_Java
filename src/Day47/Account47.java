6a11e5ba01678b0fb18a5cbcf06ddc05bde99f00
sbychul <iamsbc0427@gmail.com>
2026-04-03 02:08:49 +0900

    Day 47: 복습 22회차: 객체 직렬화(Object Serialization)를 활용한 은행 데이터(객체, 리스트) 영구 보존 시스템 구현

---
+++ src/Day47/Account47.java
@@ -0,0 +1,60 @@
+package Day47;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+// 46일차에서 두 단어 추가됨.
+public class Account47 implements Serializable {
+    private String accountNumber;
+    private String ownerName;
+    private long balance;
+    // 46일차 추가) 계좌 거래 내역을 담을 ArrayList 추가
+    private ArrayList<String> history = new ArrayList<>();
+
+    // 생성자, 거래 내역을 생성 시 써 줄 필요는 없으니 수정 X
+    public Account47(String accountNumber, String ownerName, long balance) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    public void deposit(long amount) { // 입금
+        balance += amount;
+        // 46일차 추가) 거래 내역에 입금 내역(문자열)을 추가.
+        history.add("[입금] ➕ " + amount + "원 (잔액: " + balance + "원)");
+    }
+
+    public boolean withdraw(long amount) { // 출금
+        if (balance < amount) { return false; } // 돈이 없는데 뺄 순 없잖아요
+        balance -= amount;
+        // 46일차 추가) 거래 내역에 출금 내역(문자열)을 추가.
+        history.add("[출금] ➖ " + amount + "원 (잔액: " + balance + "원)");
+        return true;
+    }
+
+    // 46일차 추가) 거래 내역을 출력하는 메서드.
+    public void printHistory() {
+        if (history.isEmpty()) { // 거래 내역이 비어 있다면
+            System.out.println("거래 내역이 없습니다.");
+            return;
+        }
+        // 안 비어 있다면 (Guard clause)
+        for (String log : history) {
+            // 한 줄씩 출력
+            System.out.println(log);
+        }
+    }
+
+    // Getter
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+}
